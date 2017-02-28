232d9d0f856fb297b122a88b1c53cc3379a75ef4
JYLiu611 <j.y_liu@hotmail.com>
2017-02-28 22:46:20 +0800

    0228

--- ex1.py
+++ ex1.py
@@ -1,9 +1,2 @@
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-print 'Yay!Printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not tough this.'
-#print
-#print "Hello World!",
+print "111"
+print "111"
